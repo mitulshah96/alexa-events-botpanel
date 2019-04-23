7ff5415e11d75154ff1be568927142933b8fc3a6
mitulshah96 <mitulshah96@gmail.com>
2019-04-23 17:10:00 +0530

    removed urls

--- config/index.js
+++ config/index.js
@@ -1,51 +1,51 @@
- (function() {
-    'use strict';
+(function() {
+  'use strict';
 
-    process.env.NODE_ENV = process.env.NODE_ENV || 'local';
-    process.env.PORT = process.env.PORT || 3008;
+  process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+  process.env.PORT = process.env.PORT || 3008;
 
-    let config = {
+  let config = {
+    web_server: {
+      host: process.env.HOST || '127.0.0.1',
+      port: process.env.PORT
+    },
+    api_base_url: 'https://graph.microsoft.com/v1.0',
+    database: {
+      connect_uri: 'mongodb://127.0.0.1/adminpanel'
+    },
+    notification: {
+      service: 'gmail',
+      username: '',
+      password: '',
+      debug_to_email: 'vaibhav.satam@accionlabs.com'
+    },
+    jwt_secret: 'MachineLearn'
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    config = {
       web_server: {
         host: process.env.HOST || '127.0.0.1',
-        port: process.env.PORT,
+        port: process.env.PORT
       },
-      api_base_url: 'https://graph.microsoft.com/v1.0',
+      api_base_url: 'https://graph.microsoft.com/beta/',
       database: {
-        connect_uri: 'mongodb://127.0.0.1/adminpanel',
+        connect_uri: 'mongodb://mongo/adminpanel'
       },
       notification: {
         service: 'gmail',
-        username: 'aic.admin@accionlabs.com',
-        password: 'aicadmin@',
-        debug_to_email: 'vaibhav.satam@accionlabs.com'
+        username: '',
+        password: ''
       },
       jwt_secret: 'MachineLearn'
     };
+  }
 
-    if (process.env.NODE_ENV === 'production') {
-      config = {
-        web_server: {
-          host: process.env.HOST || '127.0.0.1',
-          port: process.env.PORT
-        },
-        api_base_url: 'https://graph.microsoft.com/beta/',
-        database: {
-          connect_uri: 'mongodb://mongo/adminpanel'
-        },
-        notification: {
-          service: 'gmail',
-          username: 'aic.admin@accionlabs.com',
-          password: 'aicadmin@'
-        },
-        jwt_secret: 'MachineLearn'
-      };
-    }
+  config.web_server.url =
+    'http://' + config.web_server.host + ':' + config.web_server.port + '/';
 
-    config.web_server.url = 'http://' + config.web_server.host + ':' + config.web_server.port + '/';
+  console.log('Config Environment : ' + process.env.NODE_ENV);
+  console.log('Config Host : ' + process.env.HOST);
 
-    console.log('Config Environment : ' + process.env.NODE_ENV);
-    console.log('Config Host : ' + process.env.HOST);
-
-    module.exports = config;
+  module.exports = config;
 })();
-
